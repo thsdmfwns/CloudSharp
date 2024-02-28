6eae680e517f56a6f00b7ca861257df96a5b2dad
son <son2044@gmail.com>
2024-02-28 19:51:24 +0900

    add MySql and Data seeding

--- src/CloudSharp.Api/Repository/DatabaseContext.cs
+++ src/CloudSharp.Api/Repository/DatabaseContext.cs
@@ -7,4 +7,30 @@ public class DatabaseContext : DbContext
 {
     public DbSet<Member> Members { get; set; }
     public DbSet<MemberRole> MemberRoles { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL("server=localhost;port=11001;database=library;user=root;password=q1w2e3r4");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var members = modelBuilder.Entity<Member>();
+        var memberRoles = modelBuilder.Entity<MemberRole>();
+
+        
+        //seeding
+        memberRoles.HasData(
+            new MemberRole
+            {
+                Id = 1,
+                Name = "admin",
+            },
+            new MemberRole
+            {
+                Id = 2,
+                Name = "member",
+            }
+            );
+    }
 }
