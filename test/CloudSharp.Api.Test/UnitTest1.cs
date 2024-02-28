88fde4ddcbbbb7b01e853a581d73f9fe3c3f201f
son <son2044@gmail.com>
2024-02-28 18:47:48 +0900

    refactoring directory structure

---
+++ test/CloudSharp.Api.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CloudSharp.Api.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
