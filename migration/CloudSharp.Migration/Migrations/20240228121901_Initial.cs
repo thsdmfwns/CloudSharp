e71178143673fb0345d21988ff7d8d9c35160030
son <son2044@gmail.com>
2024-03-06 09:15:29 +0900

    add migration

---
+++ migration/CloudSharp.Migration/Migrations/20240228121901_Initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CloudSharp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "MemberRoles",
+                columns: table => new
+                {
+                    Id = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MemberRoles", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    LoginId = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Password = table.Column<string>(type: "longtext", nullable: false),
+                    RoleId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    Nickname = table.Column<string>(type: "longtext", nullable: false),
+                    ProfileImageId = table.Column<Guid>(type: "char(36)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    LastAccessed = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberId);
+                    table.ForeignKey(
+                        name: "FK_Members_MemberRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "MemberRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "MemberRoles",
+                columns: new[] { "Id", "CreatedOn", "Name", "UpdatedOn" },
+                values: new object[,]
+                {
+                    { 1ul, new DateTime(2024, 2, 28, 12, 19, 0, 181, DateTimeKind.Utc).AddTicks(9466), "admin", null },
+                    { 2ul, new DateTime(2024, 2, 28, 12, 19, 0, 181, DateTimeKind.Utc).AddTicks(9472), "member", null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_LoginId",
+                table: "Members",
+                column: "LoginId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_RoleId",
+                table: "Members",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "MemberRoles");
+        }
+    }
+}
