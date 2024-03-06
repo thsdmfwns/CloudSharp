ae7f8f0a6f4f62fbd6eb3f0f64cfbdde215ec3e7
son <son2044@gmail.com>
2024-03-06 09:20:05 +0900

    change to set password

--- test/CloudSharp.Api.Test/Util/EntityExtensions.cs
+++ test/CloudSharp.Api.Test/Util/EntityExtensions.cs
@@ -6,12 +6,12 @@ namespace CloudSharp.Api.Test.Util;
 
 public static class EntityExtensions
 {
-    public static Faker<Member> SetRules(this Faker<Member> faker, ulong memberRoleId)
+    public static Faker<Member> SetRules(this Faker<Member> faker, ulong memberRoleId, string password = "password")
     {
         faker
             .RuleFor(p => p.MemberId, f => Guid.NewGuid())
             .RuleFor(p => p.LoginId, f => f.Name.FirstName())
-            .RuleFor(p => p.Password, f => PasswordHasher.HashPassword(f.Internet.Password()))
+            .RuleFor(p => p.Password, f => PasswordHasher.HashPassword(password))
             .RuleFor(p => p.RoleId, memberRoleId)
             .RuleFor(p => p.Email, f => f.Internet.Email())
             .RuleFor(p => p.Nickname, f => f.Company.CompanyName())
