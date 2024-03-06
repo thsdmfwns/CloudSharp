bf4f500304c9c3ab0ead862902a840538ce17e98
son <son2044@gmail.com>
2024-03-06 09:18:20 +0900

    add MemberService Test

---
+++ test/CloudSharp.Api.Test/Util/EntityExtensions.cs
@@ -0,0 +1,21 @@
+using Bogus;
+using CloudSharp.Api.Util;
+using CloudSharp.Data.EntityFramework.Entities;
+
+namespace CloudSharp.Api.Test.Util;
+
+public static class EntityExtensions
+{
+    public static Faker<Member> SetRules(this Faker<Member> faker, ulong memberRoleId)
+    {
+        faker
+            .RuleFor(p => p.MemberId, f => Guid.NewGuid())
+            .RuleFor(p => p.LoginId, f => f.Name.FirstName())
+            .RuleFor(p => p.Password, f => PasswordHasher.HashPassword(f.Internet.Password()))
+            .RuleFor(p => p.RoleId, memberRoleId)
+            .RuleFor(p => p.Email, f => f.Internet.Email())
+            .RuleFor(p => p.Nickname, f => f.Company.CompanyName())
+            .RuleFor(p => p.ProfileImageId, f => Guid.NewGuid());
+        return faker;
+    } 
+}
