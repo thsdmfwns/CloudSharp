5324739a53e4eb5072939d05ba49ed0406eac81a
son <son2044@gmail.com>
2024-03-15 21:08:21 +0900

    make nullable and rename

--- src/CloudSharp.Api/Service/IMemberFileService.cs
+++ src/CloudSharp.Api/Service/IMemberFileService.cs
@@ -8,13 +8,13 @@ public interface IMemberFileService
 {
     Result<List<FileInfoDto>> GetFiles(Guid directoryId, string? targetFolderPath);
     Result<FileInfoDto> GetFile(Guid directoryId, string targetPath);
-    Result MoveFile(Guid directoryId, string targetPath, string toPath);
+    Result MoveFile(Guid directoryId, string targetPath, string? toFolderPath);
     Result RenameFile(Guid directoryId, string targetPath, string fileName);
     Result DeleteFile(Guid directoryId, string targetPath);
         
     Result<List<FolderInfoDto>> GetFolders(Guid directoryId, string? targetFolderPath);
     Result<FolderInfoDto> GetFolder(Guid directoryId, string targetPath);
-    Result MoveFolder(Guid directoryId, string targetPath, string toPath);
+    Result MoveFolder(Guid directoryId, string targetPath, string? toFolderPath);
     Result RenameFolder(Guid directoryId, string? path);
     Result RemoveFolder(Guid directoryId, string targetPath, string folderName);
     Result MakeFolder(Guid directoryId, string? targetPath, string folderName);
