12127c273cb60c2b8c94f1aa5f6f8061e8e83278
son <son2044@gmail.com>
2024-03-19 19:05:30 +0900

    add StringExtensions.cs

---
+++ src/CloudSharp.Api/Util/StringExtensions.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using CloudSharp.Api.Error;
+
+namespace CloudSharp.Api.Util;
+
+public static class StringExtensions
+{
+    public static bool IsFileName(this string filename)
+        => filename.IndexOfAny(Path.GetInvalidFileNameChars()) == -1 && !string.IsNullOrWhiteSpace(filename);
+
+    public static bool IsEmail(this string email)
+    {
+        var emailValidate = new EmailAddressAttribute();
+        return emailValidate.IsValid(email);
+    }
+}
