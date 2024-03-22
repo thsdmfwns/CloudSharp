4bdf3eef65b644a577662dd37781780636dbddfd
son <son2044@gmail.com>
2024-03-22 17:20:42 +0900

    add required

--- src/CloudSharp.Data/Ticket/FileStreamTicket.cs
+++ src/CloudSharp.Data/Ticket/FileStreamTicket.cs
@@ -12,7 +12,7 @@ public class FileStreamTicket : ITicket<FileStreamTicket>
     public required DirectoryType DirectoryType { get; init; }
     public required DateTimeOffset? ExpireTime { get; init; } = DateTime.Now.AddMinutes(3);
     public Guid Token { get; init; } = Guid.NewGuid();
-    public Member? TicketOwner { get; init; }
+    public required Member? TicketOwner { get; init; }
     public static FileStreamTicket? FromJson(string? json)
     {
         return json is null
