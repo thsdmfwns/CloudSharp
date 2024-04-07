f8983753ee396695b83d4e0b7918985701e31ed5
son <son2044@gmail.com>
2024-04-07 13:35:53 +0900

    remove unused

--- test/CloudSharp.Api.Test/Service/MemberTicketService.cs
+++ test/CloudSharp.Api.Test/Service/MemberTicketService.cs
@@ -50,14 +50,6 @@ public class MemberTicketService
         _filePath = _faker.MakeFakeFile(_memberDirectoryPath, null);
     }
     
-    /*
-     * ValueTask<Result<Guid>> AddFileStreamTicket(Guid memberId, string targetPath);
-       Task<Result<Guid>> AddFileUploadTicket(Guid memberId, string? targetFolderPath, string filename);
-       ValueTask<Result<T>> GetTicket<T>(Guid ticketToken) where T : ITicket<T>;
-       ValueTask<Result> DeleteTicket<T>(Guid ticketToken) where T : ITicket<T>;
-     */
-
-    
     [Test]
     [TestCase(null, null, null)] // success
     [TestCase("", null, typeof(NotFoundError))] // invalid id
