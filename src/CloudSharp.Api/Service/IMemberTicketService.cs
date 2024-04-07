85507a4d4b85118c7890e45d5967c3ddb94a62db
son <son2044@gmail.com>
2024-04-07 13:34:35 +0900

    fix typo

--- src/CloudSharp.Api/Service/IMemberTicketService.cs
+++ src/CloudSharp.Api/Service/IMemberTicketService.cs
@@ -6,7 +6,7 @@ namespace CloudSharp.Api.Service;
 
 public interface IMemberTicketService
 {
-    ValueTask<Result<Guid>> AddFileStreamTicket(Guid memberDto, string targetPath);
+    ValueTask<Result<Guid>> AddFileStreamTicket(Guid memberId, string targetPath);
     Task<Result<Guid>> AddFileUploadTicket(Guid memberId, string? targetFolderPath, string filename);
     ValueTask<Result<T>> GetTicket<T>(Guid ticketToken) where T : ITicket<T>;
     ValueTask<Result> DeleteTicket<T>(Guid ticketToken) where T : ITicket<T>;
