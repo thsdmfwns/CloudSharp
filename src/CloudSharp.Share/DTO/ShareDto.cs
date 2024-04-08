512cc4474a26902d4d9f9e60c5c4a18232e08925
son <son2044@gmail.com>
2024-04-08 19:07:33 +0900

    add property

--- src/CloudSharp.Share/DTO/ShareDto.cs
+++ src/CloudSharp.Share/DTO/ShareDto.cs
@@ -2,5 +2,8 @@ namespace CloudSharp.Share.DTO;
 
 public record ShareDto
 {
-    //todo add property
+    public string ShareId { get; init; }
+    public string MemberId { get; init; }
+    public long ExpireTime { get; init; }
+    public bool HasPassword { get; init; }
 }
