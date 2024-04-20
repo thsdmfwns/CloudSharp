7a2e90c66a35c47bc16be44e35f6d81e6ba81a6c
son <son2044@gmail.com>
2024-04-20 18:40:41 +0900

    add region

--- src/CloudSharp.Data/EntityFramework/Repository/DatabaseContext.cs
+++ src/CloudSharp.Data/EntityFramework/Repository/DatabaseContext.cs
@@ -30,17 +30,23 @@ public class DatabaseContext : DbContext
         var guildMemberRole = modelBuilder.Entity<GuildMemberRole>();
         var guildRole = modelBuilder.Entity<GuildRole>();
 
+        #region share
         share
             .HasOne(e => e.Member)
             .WithMany(e => e.Shares)
             .HasForeignKey(e => e.MemberId)
             .IsRequired();
+        #endregion
 
+        #region guild
         guild
             .HasOne(e => e.OwnMember)
             .WithMany(e => e.Guilds)
             .HasForeignKey(e => e.OwnMemberId)
             .IsRequired();
+        #endregion
+
+        #region guildChannel
 
         guildChannel
             .HasOne(e => e.Guild)
@@ -48,43 +54,64 @@ public class DatabaseContext : DbContext
             .HasForeignKey(e => e.GuildId)
             .IsRequired();
 
+        #endregion
+
+        #region guildChannelRole
+
         guildChannelRole
             .HasOne(e => e.GuildChannel)
             .WithMany(e => e.GuildChannelRoles)
             .HasForeignKey(e => e.GuildChannelId)
             .IsRequired();
+        
         guildChannelRole
             .HasOne(e => e.GuildRole)
             .WithMany(e => e.GuildChannelRoles)
             .HasForeignKey(e => e.GuildRoleId)
             .IsRequired();
 
+        #endregion
+        
+        #region guildMember
+
         guildMember
             .HasOne(e => e.Guild)
             .WithMany(e => e.GuildMembers)
             .HasForeignKey(e => e.GuildId)
             .IsRequired();
+        
         guildMember
             .HasOne(e => e.Member)
             .WithMany(e => e.GuildMembers)
             .HasForeignKey(e => e.MemberId)
             .IsRequired();
 
+        #endregion
+
+        #region guildMemberRole
+
         guildMemberRole
             .HasOne(e => e.GuildMember)
             .WithMany(e => e.GuildMemberRoles)
             .HasForeignKey(e => e.GuildMemberRoleId)
             .IsRequired();
+        
         guildMemberRole
             .HasOne(e => e.GuildRole)
             .WithMany(e => e.GuildMemberRoles)
             .HasForeignKey(e => e.GuildRoleId)
             .IsRequired();
 
+        #endregion
+
+        #region guildRole
+
         guildRole
             .HasOne(e => e.Guild)
             .WithMany(e => e.GuildRoles)
             .HasForeignKey(e => e.GuildId)
             .IsRequired();
+
+        #endregion
     }
 }
