8734bd3b387312bdcd743948e6ef01e09ffee320
son <son2044@gmail.com>
2024-04-28 20:23:03 +0900

    add FileStreamService.cs

---
+++ src/CloudSharp.Api/Service/FileStreamService.cs
@@ -0,0 +1,12 @@
+using CloudSharp.Data.Ticket;
+using FluentResults;
+
+namespace CloudSharp.Api.Service;
+
+public class FileStreamService : IFileStreamService
+{
+    public Result<FileStream> GetFileStream(FileStreamTicket fileStreamTicket)
+    {
+        throw new NotImplementedException();
+    }
+}
