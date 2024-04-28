94bd7794daa112743a16d2c96a14123e4489d920
son <son2044@gmail.com>
2024-04-28 19:11:00 +0900

    add IGuildService.cs

---
+++ src/CloudSharp.Api/Service/IGuildService.cs
@@ -0,0 +1,17 @@
+using CloudSharp.Share.DTO;
+using FluentResults;
+
+namespace CloudSharp.Api.Service;
+
+public interface IGuildService
+{
+    ValueTask<Result> CreateGuild(string guildName, Guid? guildProfileId);
+    
+    ValueTask<Result<GuildDto>> GetGuild(ulong guildId);
+    
+    ValueTask<Result> UpdateGuildName(ulong guildId, string guildName);
+    
+    ValueTask<Result> UpdateGuildProfileImage(ulong guildId, Guid? profileId);
+    
+    ValueTask<Result> DeleteGuild(ulong guildId);
+}
