6abc599cefc08be7c0e6d3d53fb36162e03f4657
son <son2044@gmail.com>
2024-04-28 19:10:05 +0900

    add migration

--- migration/CloudSharp.Migration/Migrations/DatabaseContextModelSnapshot.cs
+++ migration/CloudSharp.Migration/Migrations/DatabaseContextModelSnapshot.cs
@@ -19,6 +19,185 @@ namespace CloudSharp.Data.Migrations
                 .HasAnnotation("ProductVersion", "8.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.Guild", b =>
+                {
+                    b.Property<ulong>("GuildId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<DateTime>("CreateOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<string>("GuildName")
+                        .IsRequired()
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
+
+                    b.Property<Guid?>("GuildProfileImageId")
+                        .HasColumnType("char(36)");
+
+                    b.Property<Guid>("OwnMemberId")
+                        .HasColumnType("char(36)");
+
+                    b.Property<DateTime?>("UpdatedOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.HasKey("GuildId");
+
+                    b.HasIndex("OwnMemberId");
+
+                    b.ToTable("Guilds");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildChannel", b =>
+                {
+                    b.Property<Guid>("GuildChannelId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("char(36)");
+
+                    b.Property<DateTime>("CreateOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<string>("GuildChannelName")
+                        .IsRequired()
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
+
+                    b.Property<ulong>("GuildId")
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<DateTime?>("UpdatedOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.HasKey("GuildChannelId");
+
+                    b.HasIndex("GuildId");
+
+                    b.ToTable("GuildChannels");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildChannelRole", b =>
+                {
+                    b.Property<ulong>("GuildChannelRoleId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<DateTime>("CreateOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<Guid>("GuildChannelId")
+                        .HasColumnType("char(36)");
+
+                    b.Property<ulong>("GuildRoleId")
+                        .HasColumnType("bigint unsigned");
+
+                    b.HasKey("GuildChannelRoleId");
+
+                    b.HasIndex("GuildChannelId");
+
+                    b.HasIndex("GuildRoleId");
+
+                    b.ToTable("GuildChannelRoles");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildMember", b =>
+                {
+                    b.Property<ulong>("GuildMemberId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<DateTime>("CreateOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<ulong>("GuildId")
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<bool>("IsBanned")
+                        .HasColumnType("tinyint(1)");
+
+                    b.Property<Guid>("MemberId")
+                        .HasColumnType("char(36)");
+
+                    b.Property<string>("MemberName")
+                        .IsRequired()
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
+
+                    b.Property<DateTime?>("UpdatedOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.HasKey("GuildMemberId");
+
+                    b.HasIndex("GuildId");
+
+                    b.HasIndex("IsBanned");
+
+                    b.HasIndex("MemberId");
+
+                    b.ToTable("GuildMembers");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildMemberRole", b =>
+                {
+                    b.Property<ulong>("GuildMemberRoleId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<DateTime>("CreatedOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<ulong>("GuildMemberId")
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<ulong>("GuildRoleId")
+                        .HasColumnType("bigint unsigned");
+
+                    b.HasKey("GuildMemberRoleId");
+
+                    b.HasIndex("GuildRoleId");
+
+                    b.ToTable("GuildMemberRoles");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildRole", b =>
+                {
+                    b.Property<ulong>("GuildRoleId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<DateTime>("CreateOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<ulong>("GuildId")
+                        .HasColumnType("bigint unsigned");
+
+                    b.Property<uint>("RoleColorAlpha")
+                        .HasColumnType("int unsigned");
+
+                    b.Property<uint>("RoleColorBlue")
+                        .HasColumnType("int unsigned");
+
+                    b.Property<uint>("RoleColorGreen")
+                        .HasColumnType("int unsigned");
+
+                    b.Property<uint>("RoleColorRed")
+                        .HasColumnType("int unsigned");
+
+                    b.Property<string>("RoleName")
+                        .IsRequired()
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
+
+                    b.Property<DateTime?>("UpdatedOn")
+                        .HasColumnType("datetime(6)");
+
+                    b.HasKey("GuildRoleId");
+
+                    b.HasIndex("GuildId");
+
+                    b.ToTable("GuildRoles");
+                });
+
             modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.Member", b =>
                 {
                     b.Property<Guid>("MemberId")
@@ -30,22 +209,26 @@ namespace CloudSharp.Data.Migrations
 
                     b.Property<string>("Email")
                         .IsRequired()
-                        .HasColumnType("longtext");
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
 
                     b.Property<DateTime>("LastAccessed")
                         .HasColumnType("datetime(6)");
 
                     b.Property<string>("LoginId")
                         .IsRequired()
-                        .HasColumnType("varchar(255)");
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
 
                     b.Property<string>("Nickname")
                         .IsRequired()
-                        .HasColumnType("longtext");
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
 
                     b.Property<string>("Password")
                         .IsRequired()
-                        .HasColumnType("longtext");
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
 
                     b.Property<Guid?>("ProfileImageId")
                         .HasColumnType("char(36)");
@@ -80,7 +263,8 @@ namespace CloudSharp.Data.Migrations
                         .HasColumnType("char(36)");
 
                     b.Property<string>("Password")
-                        .HasColumnType("longtext");
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
 
                     b.Property<DateTime?>("UpdatedOn")
                         .HasColumnType("datetime(6)");
@@ -94,6 +278,96 @@ namespace CloudSharp.Data.Migrations
                     b.ToTable("Shares");
                 });
 
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.Guild", b =>
+                {
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.Member", "OwnMember")
+                        .WithMany("Guilds")
+                        .HasForeignKey("OwnMemberId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("OwnMember");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildChannel", b =>
+                {
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.Guild", "Guild")
+                        .WithMany("GuildChannels")
+                        .HasForeignKey("GuildId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Guild");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildChannelRole", b =>
+                {
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.GuildChannel", "GuildChannel")
+                        .WithMany("GuildChannelRoles")
+                        .HasForeignKey("GuildChannelId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.GuildRole", "GuildRole")
+                        .WithMany("GuildChannelRoles")
+                        .HasForeignKey("GuildRoleId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("GuildChannel");
+
+                    b.Navigation("GuildRole");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildMember", b =>
+                {
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.Guild", "Guild")
+                        .WithMany("GuildMembers")
+                        .HasForeignKey("GuildId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.Member", "Member")
+                        .WithMany("GuildMembers")
+                        .HasForeignKey("MemberId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Guild");
+
+                    b.Navigation("Member");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildMemberRole", b =>
+                {
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.GuildMember", "GuildMember")
+                        .WithMany("GuildMemberRoles")
+                        .HasForeignKey("GuildMemberRoleId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.GuildRole", "GuildRole")
+                        .WithMany("GuildMemberRoles")
+                        .HasForeignKey("GuildRoleId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("GuildMember");
+
+                    b.Navigation("GuildRole");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildRole", b =>
+                {
+                    b.HasOne("CloudSharp.Data.EntityFramework.Entities.Guild", "Guild")
+                        .WithMany("GuildRoles")
+                        .HasForeignKey("GuildId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Guild");
+                });
+
             modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.Share", b =>
                 {
                     b.HasOne("CloudSharp.Data.EntityFramework.Entities.Member", "Member")
@@ -105,8 +379,38 @@ namespace CloudSharp.Data.Migrations
                     b.Navigation("Member");
                 });
 
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.Guild", b =>
+                {
+                    b.Navigation("GuildChannels");
+
+                    b.Navigation("GuildMembers");
+
+                    b.Navigation("GuildRoles");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildChannel", b =>
+                {
+                    b.Navigation("GuildChannelRoles");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildMember", b =>
+                {
+                    b.Navigation("GuildMemberRoles");
+                });
+
+            modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.GuildRole", b =>
+                {
+                    b.Navigation("GuildChannelRoles");
+
+                    b.Navigation("GuildMemberRoles");
+                });
+
             modelBuilder.Entity("CloudSharp.Data.EntityFramework.Entities.Member", b =>
                 {
+                    b.Navigation("GuildMembers");
+
+                    b.Navigation("Guilds");
+
                     b.Navigation("Shares");
                 });
 #pragma warning restore 612, 618
