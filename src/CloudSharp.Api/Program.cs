c6023a713c4b425d1704178a50a406ee831db18f
son <son2044@gmail.com>
2024-05-01 09:49:34 +0900

    add services

--- src/CloudSharp.Api/Program.cs
+++ src/CloudSharp.Api/Program.cs
@@ -1,9 +1,11 @@
+using CloudSharp.Api.Service;
 using CloudSharp.Data.EntityFramework;
+using CloudSharp.Data.Repository;
+using CloudSharp.Data.Store;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +17,21 @@ builder.Services.AddDbContext<DatabaseContext>(
             b => b.MigrationsAssembly("CloudSharp.Migration"));
     }
 );
+
+//repository
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.AddScoped<IShareRepository, ShareRepository>();
+
+//store
+builder.Services.AddSingleton<IFileStore, FileStore>();
+builder.Services.AddSingleton<ITicketStore, HashMapTicketStore>(); //HashMap
+
+//service
+builder.Services.AddScoped<IFileStreamService, FileStreamService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IMemberFileService, MemberFileService>();
+builder.Services.AddScoped<IMemberTicketService, MemberTicketService>();
+builder.Services.AddScoped<IShareService, ShareService>();
 
 var app = builder.Build();
 
