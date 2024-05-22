bf1942eb5ae047e0ac16cc024de4d3e37c71c8c2
son <son2044@gmail.com>
2024-05-22 21:13:04 +0900

    add note

--- src/CloudSharp.Api/Service/IGuildMemberService.cs
+++ src/CloudSharp.Api/Service/IGuildMemberService.cs
@@ -8,6 +8,8 @@ public interface IGuildMemberService
     ValueTask<Result<ulong>> AddGuildMember(Guid memberId, ulong guildId, string memberName);
     ValueTask<Result<GuildMemberDto>> GetGuildMember(ulong guildMemberId);
     ValueTask<Result<GuildMemberDto>> GetOwnerGuildMember(ulong guildId);
+    //todo 밴의 방식 변경? 
+    // 밴에 대한 테이블을 하나 놔서 bool 형식이 아닌 dateTIme 형식으로
     ValueTask<Result> BanGuildMember(ulong guildMemberId);
     ValueTask<Result> UpdateGuildMemberName(ulong guildMemberId, string guildMemberName);
     ValueTask<Result> ChangeGuildOwner(ulong ownerGuildMemberId, ulong destinyGuildMemberId);
